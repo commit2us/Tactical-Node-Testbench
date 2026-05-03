3d22c4fedeed3b87ea28bf71af067fa49314e754
Alonso GT <algabtel@gmail.com>
2026-05-02 21:21:42 -0600

    feat: create header and implementation for link class. Placeholders for base.h entities

---
+++ link.cpp
@@ -0,0 +1,14 @@
+#include "link.h"
+
+Link::Link(Base* from, Base* to) : 
+bases{from, to}, distance(0.0){
+    distance = distanceFromCoordinates(from->location, to->location);
+}
+    
+Link::~Link() {
+    // No use for now
+}
+
+float Link::distanceFromCoordinates(Location from, Location to) {
+    return hypot(from.x - to.x, from.y - to.y);
+}
