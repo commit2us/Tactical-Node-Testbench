b6740827cce7188fd41e9c3b427ccf330a1e7e51
mariana <mariana.soto4940@alumnos.udg.mx>
2026-05-04 12:41:23 -0600

    añadiendo el riskMatrix.h

--- RiskMatrix.cpp
+++ RiskMatrix.cpp
@@ -1,27 +1,20 @@
-class RiskMatrix
-{
-    private:
-        float dangerZone[10][10];
+#include "RiskMatrix.h"
 
-    public:
-
-        RiskMatrix()
-        {
-            for(int i = 0; i < 10; i++)
-                for(int j = 0; j < 10; j++)
-                    dangerZone[i][j] = 1.0;
-
-            dangerZone[5][5] = 8.5;
-
+RiskMatrix::RiskMatrix() {
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
+            dangerZone[i][j] = 1.0;
         }
+    }
+    dangerZone[5][5] = 8.5;
+}
 
-        float getRiskAt(int x, int y)
-        {
-            int sectorX = x / 100;
-            int sectorY =  y /100;
-            if(sectorX > 9)sectorX = x;
-            if (sectorY > 9) sectorY = y;
+float RiskMatrix::getRiskAt(int x, int y) {
+    int sectorX = x / 100;
+    int sectorY = y / 100;
+    
+    if(sectorX > 9) sectorX = 9;
+    if(sectorY > 9) sectorY = 9;
 
-        }
-        
-};
+    return dangerZone[sectorX][sectorY]; 
+}
