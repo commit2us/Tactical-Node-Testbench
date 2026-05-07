a26599b46bf0a35b59103207f7dbd09455cffef2
Alonso GT <algabtel@gmail.com>
2026-05-07 13:07:41 -0600

    feat: utility functions

--- Network.cpp
+++ Network.cpp
@@ -1,10 +1,19 @@
 #include "Network.h"
+#include <optional>
 #include <vector>
 
 using namespace std;
 
 Network::Network(vector<Base*> bases) {
     // TODO
+}
+
+inline Base* Network::baseExists(int baseId) const {
+    auto base =  orderToBase.find(baseId);
+
+    if (base == orderToBase.end()) return nullptr;
+
+    return base->second;
 }
 
 float inline Network::findLink(int baseNode, int linkedNode) const {
@@ -17,5 +26,16 @@ float inline Network::findLink(int baseNode, int linkedNode) const {
     if (link == node->second.end()) return -1;
             
     return link->second;
+}
 
+inline string Network::getBaseIdByOrder(int order) const {
+    Base* base;
+
+    if (!(base = baseExists(order))) return "";
+
+    return base->getId();
+}
+
+inline Base* Network::getBaseByID(int baseId) const {
+    return baseExists(baseId);
 }
--- Network.h
+++ Network.h
@@ -3,26 +3,28 @@
 
 #include "Base.h"
 #include "Link.h"
-#include <optional>
+
 #include <unordered_map>
-#include <unordered_set>
 #include <vector>
 
 class Network{
 private:
     // Lightweigh translation from Base objects to O(1) access structures
     std::unordered_map<int, std::unordered_map<int, float>> graph;
-    std::unordered_map<int, Base*> idBases;
-    std::unordered_map<std::string, int> basesDescriptionId;
+    std::unordered_map<int, Base*> orderToBase;
+    std::unordered_map<std::string, int> baseIdToOrder;
 
 public:
+    // Translate Base objects to internal structures
     Network(std::vector<Base*> bases);
+
+    inline Base* baseExists(int baseId) const;
 
     // Return the cost of the link if found, -1 if not
     inline float findLink(int baseNode, int linkedNode) const;
 
     // Return base description from the bases' map {id : obj}
-    inline string getBaseDescriptionById(int baseId) const;
+    inline std::string getBaseIdByOrder(int order) const;
 
     // Return full object from the bases' map {id : obj}
     inline Base* getBaseByID(int baseId) const;
